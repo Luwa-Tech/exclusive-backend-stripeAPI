a895e35f054b5b7b038d777578052518d5b52a5d
yusuf umar faruq <luwaservices@gmail.com>
2023-08-07 11:28:32 -0700

    Added corsOptions logic in config folder

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //sk_test_51NbkexEQoG2EqoC4ZNnnQePfWgaeB4Kn06K8mJj6WfJknVoXc3TpbzGkeAIOOHwp3Z7YVUC8jD774DHtBsskgWJy00bHaMWBJ6
 const express = require("express")
 const server = express()
+const corsOptions = require("./config/corsOptions")
 const cors = require("cors")
 
 const PORT = process.env.PORT || 3500
 
-server.use(cors())
+server.use(cors(corsOptions))
 server.use(express.static("public"))
 server.use(express.json())
 
