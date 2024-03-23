a586bd7dee300a35b163a1425fd3e9f8c74e8d18
Umar Yusuf <luwaservices@gmail.com>
2024-03-23 21:46:58 +0100

    Fixed multer undefined file path

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,19 +1,6 @@
 const multer = require("multer");
-const path = require("path");
 
 module.exports = multer({
   storage: multer.diskStorage({}),
-  fileFilter: async (req, file) => {
-    try {
-      let ext = path.extname(file.originalname);
-      if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-        throw new Error("File type is not supported");
-      }
-      return true;
-    } catch (error) {
-      return false;
-    }
-  },
+  limits: { fileSize: 500000 }
 });
-
-//router.post("/createPost", upload.single("file"), postsController.createPost);
