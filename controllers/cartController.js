b1268002664a70e3ba8ffe7a61fed12bd2d72b0f
Umar Yusuf <luwaservices@gmail.com>
2024-04-01 21:08:04 +0100

    Refactored cart controller

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,14 +1,13 @@
 const Cart = require("../model/Cart");
 const Product = require("../model/Product");
-const generateSessionID = require("../utils/generateSessionID");
 
 const getUserCart = async (req, res) => {
     if (!req.user) {
-        return res.sendStatus(204);
+        return res.status(401).json({"message": "Please log in to view your cart"});
     }
 
     try {
-        const cartItems = cartItems = await Cart.findOne({ user: req.user._id }).populate("items");
+        const cartItems = await Cart.findOne({ user: req.user._id }).populate("items");
 
         // Fetch product details for cart items using mongodb aggregation
         const pipeline = [
@@ -32,10 +31,9 @@ const getUserCart = async (req, res) => {
 
         console.log(products)
 
-        res.json({ "cart": products });
+        res.status(200).json(products);
 
     } catch (err) {
-        console.log(err.message)
         res.status(500).json({ "message": "Internal server error" });
     }
 }
@@ -43,36 +41,66 @@ const getUserCart = async (req, res) => {
 const addToCart = async (req, res) => {
     const { productId } = req.body;
 
-    if (!req.user) {
-        return res.status(400).json({"message": "User must be logged in"});
-    }
+    // if (!req.user) {
+    //     return res.status(401).json({"message": "User must be logged in"});
+    // }
 
     if (!productId) {
         return res.status(400).json({ "message": "ProductId is required" });
     }
 
     try {
-        const userCart = await Cart.findOneAndUpdate(
-            { user: req.user._id },
-            { $addToSet: { items: { product: productId } } },
-            { upsert: true, new: true }
-        );
+        let responseMessage;
+        const userCart = await Cart.findOne({ user: req.user._id});
+        const isItemInCart = userCart.items.find(items => items.product === productId);
+        
+        if (!isItemInCart) {
+            userCart.items.push({
+                product: productId,
+                quantity: 1
+            })
+            responseMessage = "Product added to cart";
+        } else {
+            isItemInCart += 1;
+            responseMessage = "Product quantity updated";
+        }
 
-        res.json({ "message": "Product added to cart", "cartItems": userCart })
+        await userCart.save();
+        res.json({"message": responseMessage});
 
-    } catch (err) {
-        console.log(err)
+    } catch (err){
         res.status(500).json({ "message": "Internal server error" });
     }
 }
 
+const decreaseItemQty = async (req, res) => {
+    const {productId} = req.body;
+
+    // if (!req.user) {
+    //     return res.status(401).json({"message": "User must be logged in"})
+    // }
+
+    if (!productId) {
+        return res.status(400).json({ "message": "ProductId is required" });
+    }
+
+    try {
+        const userCart = await Cart.findOne({user: req.user._id});
+        const item = userCart.items.find(item => item.product === productId);
+        item.quantity -= 1;
+        await userCart.save();
+        res.json({"message": "Product quantity updated"});
+    } catch (err) {
+        res.status(500).json({"message": "Internal server error"});
+    }
+}
 
 const removeFromCart = async (req, res) => {
     const { productId } = req.body;
 
-    if (!req.user) {
-        return res.status(400).json({"message": "User must be logged in"})
-    }
+    // if (!req.user) {
+    //     return res.status(401).json({"message": "User must be logged in"})
+    // }
 
     if (!productId) {
         return res.status(400).json({ "message": "ProductId is required" });
@@ -86,7 +114,6 @@ const removeFromCart = async (req, res) => {
         res.json({ "message": "Product has been removed from cart" });
 
     } catch (err) {
-        console.log(err)
         res.status(500).json({ "message": "Internal server error" });
     }
 }
@@ -94,5 +121,6 @@ const removeFromCart = async (req, res) => {
 module.exports = {
     getUserCart,
     addToCart,
+    decreaseItemQty,
     removeFromCart
 }
--- routes/cart.js
+++ routes/cart.js
@@ -3,8 +3,9 @@ const router = express.Router();
 // const isUserAuthenticated = require("../middleware/isAuthenticated")
 const cartController = require("../controllers/cartController");
 
-router.get("/", cartController.getUserCart);
-router.put("/add", cartController.addToCart);
-router.put("/remove", cartController.removeFromCart);
+router.get("/", cartController.getUserCart)
+router.put("/add", cartController.addToCart)
+router.put("/decrease-item-qty", cartController.decreaseItemQty)
+router.put("/remove", cartController.removeFromCart)
 
 module.exports = router;
