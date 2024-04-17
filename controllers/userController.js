74f2444366a8cb9df6b164435a8b889cae9bce73
Umar Yusuf <luwaservices@gmail.com>
2024-04-17 22:07:14 +0100

    Removed passport implementation

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,13 +23,6 @@ const handleSignup = async (req, res) => {
         });
 
         await user.save();
-
-        req.logIn(user, (err) => {
-            if (err) {
-              return next(err);
-            }
-            res.status(201).json({"message": "New user created and logged in successfully"})
-          });
     } catch (err) {
         res.status(500).json({"message": `${err.message}`});
     }
@@ -41,22 +34,6 @@ const handleLogin = async (req, res, next) => {
     if (!email || !password) {
         return res.status(400).json({"message": "Email and Password is required!"});
     }
-
-    passport.authenticate("local", (err, user, info) => {
-        if (err) {
-            return next(err);
-          }
-          if (!user) {
-            return res.status(401).json({ "message": "Invalid Email or Password." });
-          }
-  
-          req.logIn(user, (err) => {
-            if (err) {
-              return next(err);
-            }
-            res.status(202).json({"message": "User logged in successfully", "user": user})
-          });
-    })(req, res, next);
 }
 
 module.exports = {handleSignup, handleLogin};
