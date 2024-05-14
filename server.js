f6fea4f3a50e1c0adcdd30efb7382240af30d1b1
Umar Yusuf <luwaservices@gmail.com>
2024-05-14 09:31:00 +0100

    Auth0 integration

--- server.js
+++ server.js
@@ -6,20 +6,27 @@ const corsOptions = require("./config/corsOptions");
 const credentials = require("./middleware/credentials");
 const cors = require("cors");
 const connectDB = require("./config/dbConn");
-
-require("./config/passport")(passport);
-
-connectDB();
+const { auth } = require("express-oauth2-jwt-bearer");
 
 const PORT = process.env.PORT || 3500;
+
+const jwtCheck = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_DOMAIN,
+  tokenSigningAlg: "RS256"
+});
+
+connectDB();
 
 server.use(credentials);
 server.use(cors(corsOptions));
 server.use(express.static("public"));
 server.use(express.json());
 
-server.use("/", require("./routes/user"));
 server.use("/products", require("./routes/product"));
+
+server.use(jwtCheck);
+
 server.use("/api/user/wishlist", require("./routes/wishlist"));
 server.use("/api/user/cart", require("./routes/cart"));
 server.use("/api/user/checkout", require("./routes/checkout"));
